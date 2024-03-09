dcf737c0a48d9ce3addeb38c0023147cfebd5375
Adnan Humačkić <humackic.adnan@gmail.com>
2024-03-09 22:11:14 +0100

    migracije

--- frontend/src/app/profesor-pocetna/profesor-pocetna.component.ts
+++ frontend/src/app/profesor-pocetna/profesor-pocetna.component.ts
@@ -3,66 +3,69 @@ import {HttpClient} from "@angular/common/http";
 import {ActivatedRoute, Router} from "@angular/router";
 import {NgForOf, NgIf} from "@angular/common";
 import Chart from 'chart.js/auto';
+import {MojConfig} from "../moj-config";
+import {GetPodaciByProffRespo} from "./podaci-by-profesor";
 
 @Component({
   selector: 'app-profesor-pocetna',
   standalone: true,
-    imports: [
-        NgForOf,
-        NgIf,
-    ],
+  imports: [
+    NgForOf,
+    NgIf,
+  ],
   templateUrl: './profesor-pocetna.component.html',
   styleUrl: './profesor-pocetna.component.css'
 })
 export class ProfesorPocetnaComponent implements OnInit {
   private id: number | undefined;
   public razred: any;
-  otvorenModal:boolean=false;
+  otvorenModal: boolean = false;
 
-  constructor(private client:HttpClient, private route: ActivatedRoute, private router:Router) {
+  constructor(private client: HttpClient, private route: ActivatedRoute, private router: Router) {
   }
 
 
   ngOnInit(): void {
-    this.id=parseInt(localStorage!.getItem("id")!);
+    this.id = parseInt(localStorage!.getItem("id")!);
 
     this.UcitajRazrede();
-    this.createChart();
+    //this.createChart(pod.prosjecnaProlaznost, pod.brojStudenata);
 
   }
 
-  createChart() {
-    var myChart = new Chart('vrijeme', {
+  createChart(prosjecnaProlaznost: number, brojStudenata: number, ukupnoBodova: number) {
+    console.log(prosjecnaProlaznost, brojStudenata, ukupnoBodova);
+    var myChart = new Chart('prolaznost', {
       type: 'bar',
       data: {
-        labels: [123,321,11],
+        labels: [prosjecnaProlaznost],
         datasets: [
           {
-            label: 'Minutes',
-            data: [11,22,33],
-            type: 'bar', // Ovo postavlja tip na bar za glavni set podataka
+            label: 'Izlaznost',
+            data: brojStudenata,
+            type: 'bar',
             borderWidth: 2,
-            order: 2, // Redosled crtanja za barove
-            backgroundColor: 'rgba(75, 192, 192, 0.2)', // Boja ispune barova
-            borderColor: 'rgba(75, 192, 192, 1)', // Boja linija oko barova
+            order: 2,
+            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+            borderColor: 'rgba(75, 192, 192, 1)',
           },
           {
-            type: 'line', // Ovo postavlja tip na line za dodatni set podataka
-            label: 'Minutes line',
-            data: [123,32,1], // Podaci za dodatni set (može biti drugačiji niz podataka ako je potrebno)
-            fill: false, // Da li popuniti prostor ispod linije
-            order: 1, // Redosled crtanja za liniju
-            borderColor: 'rgba(255, 99, 132, 1)', // Boja linije
-            tension: 0.1, // Tenzija linije (0.0 - 1.0)
+            type: 'line',
+            label: 'Prolaznost',
+            data: prosjecnaProlaznost,
+            fill: false,
+            order: 1,
+            borderColor: 'rgba(255, 99, 132, 1)',
+            tension: 0.1,
 
           },
         ],
       },
       options: {
-        plugins:{
-          title:{
-            display:true,
-            text:'Sitting time per day'
+        plugins: {
+          title: {
+            display: true,
+            text: 'Sitting time per day'
           }
         },
         scales: {
@@ -73,20 +76,21 @@ export class ProfesorPocetnaComponent implements OnInit {
       },
     });
   }
-  private UcitajRazrede() {
-    this.client.get(`https://localhost:7020/Razredi/byProfesor?id=${this.id}`, {observe:'response'}).subscribe((data)=>{
-      if(data.status==200)
-      {
-        this.razred=data.body
-      }
+
+  podaci: GetPodaciByProffRespo[] = [];
+
+  UcitajRazrede() {
+    let url = MojConfig.adresa_servera + `/GrafPodaciProfesor?profesorId=${this.id}`;
+    this.client.get<GetPodaciByProffRespo[]>(url).subscribe(x => {
+      this.podaci = x;
     })
   }
 
   otvoriModal() {
-    this.otvorenModal=true;
+    this.otvorenModal = true;
   }
 
   zatvoriModal() {
-    this.otvorenModal=false;
+    this.otvorenModal = false;
   }
 }
