c121f02edc4c4ef128f956e4a2499ca345dbba97
Armin Đidelija <didelija.armin@gmail.com>
2024-03-09 23:51:30 +0100

    update predmeti controller

--- Backend/HackathonBest24/Hackathon.API/Controllers/PredmetiController.cs
+++ Backend/HackathonBest24/Hackathon.API/Controllers/PredmetiController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using static Hackathon.API.Controllers.AiPreporukaOblastiController;
 
 namespace Hackathon.API.Controllers
 {
@@ -16,16 +17,45 @@ namespace Hackathon.API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult> GetAll()
+        public async Task<ActionResult> GetAll([FromQuery]int studentId)
         {
-            var predmeti = _applicationDbContext
-                .Predmet
-                .OrderBy(x=>x.Naziv)
-                .ToList();
+            var studentiOdgovori = _applicationDbContext.StudentiTestoviOdgovori
+               .Include(x => x.StudentiTestovi)
+               .Include(x => x.Odgovor)
+               .Include(x => x.Odgovor.Pitanje)
+               .Include(x => x.Odgovor.Pitanje.Oblast)
+               .Where(x=>x.StudentiTestovi.StudentId==studentId)
+               .ToList();
 
-            return Ok(predmeti);
+            var rezultatiPredmeta = studentiOdgovori
+            .GroupBy(so => so.Odgovor.Pitanje.Oblast.PredmetId) // Grupiraj prema PredmetId
+            .Select(group => new
+            {
+                PredmetId = group.Key,
+                //NazivPredmeta = group.First().Odgovor.Pitanje.Oblast.Predmet.Naziv, // Dodano polje za naziv predmeta
+                ProsjecnaTacnost = group.Average(so => so.Odgovor.Tacan ? 1.0 : 0.0) * 100
+            }).ToList();
+
+            var podaci = new List<PredmetiHumaDto>();
+
+            foreach(var rp in rezultatiPredmeta)
+            {
+                podaci.Add(new PredmetiHumaDto()
+                {
+                    Predmet = _applicationDbContext.Predmet.Find(rp.PredmetId),
+                    Uspjesnost = rp.ProsjecnaTacnost
+                });
+            }
+
+            return Ok(podaci);
         }
 
 
+    }
+
+    public class PredmetiHumaDto
+    {
+        public Predmet Predmet { get; set; }
+        public double Uspjesnost { get; set; }
     }
 }
