28b4eb9fdfa39bfc9558d4437d5abdb8d7170ca2
Adnan Humačkić <humackic.adnan@gmail.com>
2024-03-09 02:03:09 +0100

    dodane dvije nove komponente

---
+++ frontend/src/app/prijava/prijava.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PrijavaComponent } from './prijava.component';
+
+describe('PrijavaComponent', () => {
+  let component: PrijavaComponent;
+  let fixture: ComponentFixture<PrijavaComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [PrijavaComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(PrijavaComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
