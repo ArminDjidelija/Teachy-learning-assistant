ea0d65907e3bf1a5b3fcd35bb0330dc33796db3b
Adnan Humačkić <humackic.adnan@gmail.com>
2024-03-09 04:56:14 +0100

    nova komp

---
+++ frontend/src/app/testovi-student/testovi-student.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-testovi-student',
+  standalone: true,
+  imports: [],
+  templateUrl: './testovi-student.component.html',
+  styleUrl: './testovi-student.component.css'
+})
+export class TestoviStudentComponent {
+
+}
