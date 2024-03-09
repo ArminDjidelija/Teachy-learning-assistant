8823c47b5d102096e3ee5275f055de5ddefb02a8
Armin Đidelija <didelija.armin@gmail.com>
2024-03-09 03:41:54 +0100

    kreiran kontroler pocetni

---
+++ Backend/HackathonBest24/Hackathon.API/Controllers/RazrediController.cs
@@ -0,0 +1,30 @@
+﻿using Hackathon.API.Modeli;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hackathon.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class RazrediController : ControllerBase
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+        public RazrediController(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            var profesor = _applicationDbContext.Profesor.FirstOrDefault();
+            var razredi = _applicationDbContext
+                .Razred
+                .Where(x=>)
+                .ToList();
+
+
+            return Ok(razredi);
+        }
+    }
+}
