f986379fc9cb4f81093cdeaf00d2d2192b1bba33
Nedim <nedim.mustafic02@gmail.com>
2024-03-09 07:25:15 +0100

    Dodani razredi
    Dodani razredi

--- frontend/src/app/profesor-page/profesor-page.component.ts
+++ frontend/src/app/profesor-page/profesor-page.component.ts
@@ -1,20 +1,33 @@
 import { Component } from '@angular/core';
 import {RouterLink, RouterOutlet} from "@angular/router";
+import {HttpClient} from "@angular/common/http";
+import {MojConfig} from "../moj-config";
+import {Razred, RazredLista} from "./get-razredi-profesor";
+import {CommonModule} from "@angular/common";
 declare function init_plugin():any;
 @Component({
   selector: 'app-profesor-page',
   standalone: true,
   imports: [
     RouterOutlet,
-    RouterLink
+    RouterLink,
+    CommonModule
   ],
   templateUrl: './profesor-page.component.html',
   styleUrl: './profesor-page.component.css'
 })
 export class ProfesorPageComponent {
-  constructor() {
+  constructor(private http:HttpClient) {
   }
   ngOnInit(): void {
     init_plugin();
+    this.getRazred();
+  }
+  razred:any;
+  getRazred(){
+    var url = MojConfig.adresa_servera+'/Razredi';
+    return this.http.get<Razred>(url).subscribe(x=>{
+      this.razred=x;
+    });
   }
 }
