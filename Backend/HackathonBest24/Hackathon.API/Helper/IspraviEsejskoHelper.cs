a19fed269d926df68bb6db4c13df34e1459d40b0
Armin Đidelija <didelija.armin@gmail.com>
2024-03-09 23:39:47 +0100

    kreiran ispravi esejsko metoda helper

---
+++ Backend/HackathonBest24/Hackathon.API/Helper/IspraviEsejskoHelper.cs
@@ -0,0 +1,41 @@
+﻿using Hackathon.API.Controllers;
+using iText.Commons.Bouncycastle.Cert.Ocsp;
+using OpenAI_API;
+
+namespace Hackathon.API.Helper
+{
+    public class IspraviEsejskoHelper
+    {
+        public IspraviEsejskoHelper()
+        {
+            
+        }
+        public async Task<int> IspraviEsejsko(IConfiguration configuration, IspraviEsejskoDto req)
+        {
+            string openaiKey = configuration.GetValue<string>("OpenAi:Key");
+            var openAi = new OpenAIAPI(new APIAuthentication(openaiKey));
+            var conversation = openAi.Chat.CreateConversation();
+
+            var requestGpt = $"Ti si pametni personalni profesor koji treba da ispravi pitanje i ocijeni realno. " +
+                             $"Dat cu ti pitanje od profesora, odgovor od profesora, odgovor od studenta i broj bodova pitanja" +
+                             $"Onda mi ti trebas realno bodovati taj odgovor studenta, na osnovu njegovog odgovora" +
+            $"Ovo je pitanje: {req.Pitanje}." +
+            $"Ovo je odgovor profesora, odnosno tačni odgovor: {req.OdgovorTacan}. " +
+                             $"Ovo je odgovor studenta koji trebaš odgovoriti: {req.OdgovorKorisnik}. " +
+                             $"Broj bodova pitanja je {req.Bodovi}" +
+                             $"Trebaš mi samo vratiti u broju broj bodova koje bi mu ti dao, znaci samo broj bez tacke, bez icega." +
+                             $"Ocjenjuj realno, odnosno korisnik ne mora reći u potpunosti isto, već samo da je tačno. " +
+                             $"Može student i proširiti i skratiti svoj odgovor, ali ako se vidi da student razmije definiciju daj mu sve bodove. " +
+                             $"Ako ima greške skini mu shodno bodove";
+
+
+
+            conversation.AppendUserInput(requestGpt);
+            var response = await conversation.GetResponseFromChatbotAsync();
+
+            var bodovi = int.Parse(response);
+
+            return bodovi;
+        }
+    }
+}
