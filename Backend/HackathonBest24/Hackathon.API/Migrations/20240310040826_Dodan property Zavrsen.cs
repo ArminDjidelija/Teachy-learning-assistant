82dc82fd0ab50e2d435cc74293a6b05480d2b9f2
Zaim Mehic <zaim.mehic_7@hotmail.com>
2024-03-10 05:13:40 +0100

    Ispravka za test zavrsen

---
+++ Backend/HackathonBest24/Hackathon.API/Migrations/20240310040826_Dodan property Zavrsen.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hackathon.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class DodanpropertyZavrsen : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Zavrsen",
+                table: "Test",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Zavrsen",
+                table: "Test");
+        }
+    }
+}
